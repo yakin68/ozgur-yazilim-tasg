09002698a02449393cc64e168af3cd1d54888571
yakin68 <yakin68@gmail.com>
2024-05-04 00:55:04 +0300

    new

--- jenkins/package-with-maven-container.sh
+++ jenkins/package-with-maven-container.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $HOME/.m2:/root/.m2 -v $WORKSPACE:/app -w /app maven:3.9.5-amazoncorretto-17 mvn clean package
+docker run --rm -v $HOME/target:/root/target -v $WORKSPACE:/app -w /app maven:3.9.5-amazoncorretto-17 mvn clean package
